68a8a38ae0d3bddfc56d8516eaf17a8ba5f0c83e
Danila <Danila@DESKTOP-44LP0PV>
2026-06-24 18:07:40 +0300

    1

--- WorkPracticeLauncher/Feedback.cs
+++ WorkPracticeLauncher/Feedback.cs
@@ -21,7 +21,7 @@ namespace WorkPracticeLauncher
 	public static class FeedbackManager
 	{
 		// Конфигурация GitHub API
-		private const string GITHUB_TOKEN = "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";   // Токен доступа
+		private const string GITHUB_TOKEN = "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";   // Токен доступа
 		private const string REPO_OWNER = "OneWayToDie";                                    // Владелец репозитория
 		private const string REPO_NAME = "Work_Practice";                                   // Имя репозитория
 		private const string FILE_PATH = "WorkPracticeLauncher/reviews.json";               // Путь к файлу в репозитории
